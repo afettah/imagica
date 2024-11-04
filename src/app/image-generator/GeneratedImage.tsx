d76eb5b8a70aabdef2f3c510becaa4eca38d1606
aghazi <afettah.ghazi@gmail.com>
2024-11-05 00:50:03 +0100

    add loading message

--- src/app/image-generator/GeneratedImage.tsx
+++ src/app/image-generator/GeneratedImage.tsx
@@ -4,20 +4,26 @@ import { Card, CardContent, CardHeader } from '@/components/ui/card';
 
 interface GeneratedImageProps {
   imageUrl?: string;
+  loading: boolean;
 }
 
-export const GeneratedImage: React.FC<GeneratedImageProps> = ({ imageUrl }) => {
+export const GeneratedImage: React.FC<GeneratedImageProps> = ({ imageUrl, loading }) => {
   return (
     <Card>
       <CardHeader>
         <h2 className="text-2xl font-bold mb-4">Generated Image</h2>
       </CardHeader>
       <CardContent>
-        {imageUrl ? (
+        {loading ? (
+          <div className="flex justify-center items-center space-x-2">
+            <span className="animate-pulse text-lg font-semibold">Loading</span>
+            <span className="animate-bounce text-lg font-semibold">...</span>
+          </div>
+        ) : imageUrl ? (
           <Image src={imageUrl} alt="Generated" width={500} height={500} className="w-full max-w-md mx-auto rounded-lg shadow-lg" unoptimized />
         ) : (
           <div>No image generated yet</div>
-        )}{' '}
+        )}
       </CardContent>
     </Card>
   );
--- src/app/image-generator/ImageGenerator.tsx
+++ src/app/image-generator/ImageGenerator.tsx
@@ -5,11 +5,14 @@ import { generateImage, PredictionRequest } from '../prediction.api';
 
 export const ImageGenerator: React.FC = () => {
   const [generatedImage, setGeneratedImage] = useState<string | null>(null);
+  const [loading, setLoading] = useState<boolean>(false);
 
   const onSubmit = async (request: PredictionRequest) => {
     setGeneratedImage('');
+    setLoading(true);
 
     return generateImage(request).then((url) => {
+      setLoading(false);
       setGeneratedImage(url);
     });
   };
@@ -22,7 +25,7 @@ export const ImageGenerator: React.FC = () => {
           <Form onSubmit={onSubmit} />
         </div>
         <div className="flex-1 p-6 space-y-6">
-          <GeneratedImage imageUrl={generatedImage || undefined} />
+          <GeneratedImage imageUrl={generatedImage || undefined} loading={loading} />
         </div>
       </div>
     </div>
